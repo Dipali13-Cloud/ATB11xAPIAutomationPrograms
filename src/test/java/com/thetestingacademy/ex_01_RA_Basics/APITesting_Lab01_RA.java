8fe249005da9d0b1f2ae222ca088f1cf98bfbd4b
ATB11xAPIAutomationPrograms <dipalikunwar13@gmail.com>
2025-05-17 00:07:06 +0530

    Rest Assured

--- src/test/java/com/thetestingacademy/ex_01_RA_Basics/APITesting_Lab01_RA.java
+++ src/test/java/com/thetestingacademy/ex_01_RA_Basics/APITesting_Lab01_RA.java
@@ -1,8 +1,8 @@
-package com.thetestingacademy;
+package com.thetestingacademy.ex_01_RA_Basics;
 
 import io.restassured.RestAssured;
 
-public class Lab01_RA {
+public class APITesting_Lab01_RA {
     public static void main(String[] args) {
         RestAssured.given()
                     .baseUri("https://restful-booker.herokuapp.com/ping")
